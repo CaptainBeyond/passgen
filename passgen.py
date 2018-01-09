e13d9de5b588ade2d17c7f57307c3fb7c89f136f
CaptainBeyond <vnezdd@gmail.com>
2018-01-09 11:18:47 +0200

    req

--- passgen.py
+++ passgen.py
@@ -1,20 +1,46 @@
 import telebot
+from telebot import types
 from string import digits
 from random import choice
+import pymssql
+
+con = pymssql.connect(host='den1.mssql3.gear.host',user='testbuddrugom',password='Halabuda_2017',database='testbuddrugom')
+cursor = con.cursor()
 
 TOKEN = '509247585:AAFpJ_Q2eOsn_vt7L5XWrN4beKrKfVrv9JA'
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Привет, уважаемый пользователь приложение Будь Другом, '
-                                             'для того чтобы зарегистрироваться в системе введи /signup,'
-                                             'если же ты хочешь получить код для входа в систему следует ввести /signin')
 
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
+    button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+    keyboard.add(button_phone, button_geo)
+    musor = bot.send_message(message.chat.id, text='musora',  reply_markup=keyboard)
+    # bot.send_message(message.chat.id, message.text)
+    # print(message.text)
+    # print('asdfasdf')
+    bot.register_next_step_handler(musor, checkphone)
+
+@bot.message_handler(commands=['sendphone'])
+def checkphone(message):
+    phone = bot.send_message(message.chat.id,message.contact.phone_number[2:])
+    cursor.execute("declare @check int set @check= 0 SELECT  @check = case when Count(mobile_number) is not null then 1 "
+                   "else 0 end  FROM users WHERE mobile_number = {phone_number} group by(mobile_number) select @check as"
+                   " result".format(phone_number=message.contact.phone_number[2:]))
+    result = cursor.fetchone()
+    check, = result
+    check = int(check)
+    if check == 1:
+        bot.register_next_step_handler(phone, signin)
+        signin(message)
+    else:
+        bot.send_message(message.chat.id, "Войдите в телеграм с учетной записи, номер которой зарегистрирован в приложении")
 
 @bot.message_handler(commands=['signin'])
 def signin(message):
     password = ""
-    bot.send_message(message.chat.id, 'Ваш пароль для входа в приложение:{password}'.format(name=message.text,password = password.join([choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ' + digits) for i in range(6)]) ))
+    bot.send_message(message.chat.id, 'Ваш пароль для входа в приложение:{password}'.format(password = password.join([choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ' + digits) for i in range(6)])))
 
 @bot.message_handler(commands=['signup'])
 def signup(message):
